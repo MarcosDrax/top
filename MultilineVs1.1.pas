a04b65a06f9ac8523d03a78568d8ff47ac1e8291
Marcos Vinicius Gomes <131676551+MarcosDrax@users.noreply.github.com>
2025-01-08 15:47:00 -0300

    Update and rename MultilineVs0_1.pas to MultilineVs1.1.pas

--- MultilineVs1.1.pas
+++ MultilineVs1.1.pas
@@ -1,11 +1,16 @@
 //<-----------------------------------Script Padrão INI------------------------------------------>
-//Criar um script que exiba um relatorio com o registro que estara em cada caixa e o numero da caixa, 
-//mais a quantidade total por caixa e a quantidade total do arquivo. Todos devem estar por profundidade
-//Nao usar o script padrao, criar do zero. Fazer logica que separa quantidades por caixa
+{Criar um script que exiba um relatorio com o registro que estara em cada caixa e o numero da caixa, mais a quantidade total por caixa e a quantidade total do arquivo. 
+ 
+- Todos devem estar por profundidade(OK), 
+- não usar o script padrao, criar do zero. 
+- Fazer logica que separa quantidades por caixa(OK)}
 //<-----------------------------------Script Padrão FIM------------------------------------------>
-folha := 0;
-Readln(s);
+AVS := 0;
+qtdcarne := 4;
+totFls := 0;
+flsCaixa := 1500; 
 //<-----------------------------------variaveis Multiline INI------------------------------------------>
+Readln(s);
 impr := 0;
 close := 0;
 linhas :='';
@@ -16,6 +21,7 @@ bloco_parc := 330;
 bloco_pix := 503;
 //<-----------------------------------variaveis Multiline INI------------------------------------------>
 while true do Begin
+folha := 0;
 	ident        := TrimStr(GetString(S,1,1));
 	If (ident = '1') and (impr = 1) then Begin
 	//<-----------------------------------variaveis padrão INI------------------------------------------>
@@ -42,7 +48,7 @@ while true do Begin
 		venal_predio        := getfloat(linha_taxa,30,11);
 		venal_imovel        := getfloat(linha_taxa,41,11);
 	//<-----------------------------------variaveis padrão FIM------------------------------------------>
-
+			AVS := AVS+1;
 			//demonstrativo de entrega
 			BeginPage(PAGE1);
 			ClearFields(PAGE1,REC1);
@@ -54,6 +60,7 @@ while true do Begin
 				PAGE1.REC1.ENDERECO[2]     := bairro_entrega+' '+num_entrega;
 				PAGE1.REC1.ENDERECO[3]     := cid_entrega+' '+uf_entrega;
 				PAGE1.REC1.ENDERECO[4]     := SubStr(cep_entrega,1,5)+'-'+SubStr(cep_entrega,6,3);
+				PAGE1.REC1.AVISO_TOP          := AVS;
 			WriteRecord(PAGE1,REC1);
 			EndPage(PAGE1);
 			folha := folha+1;
@@ -69,6 +76,7 @@ while true do Begin
 				PAGE2.REC1.ENDERECO[2]     := bairro_entrega+' '+num_entrega;
 				PAGE2.REC1.ENDERECO[3]     := cid_entrega+' '+uf_entrega;
 				PAGE2.REC1.ENDERECO[4]     := SubStr(cep_entrega,1,5)+'-'+SubStr(cep_entrega,6,3);
+				PAGE2.REC1.AVISO_TOP          := AVS;
 			WriteRecord(PAGE2,REC1);
 			EndPage(PAGE2);
 			folha := folha+1;
@@ -89,6 +97,7 @@ while true do Begin
 				PAGE3.REC1.VV_EDIFICA := FormatFloat(venal_predio,'#.###,##');
 				PAGE3.REC1.VV_TOTAL := FormatFloat(venal_imovel,'#.###,##');
 				PAGE3.REC1.CADASTRO := cadastro;
+				PAGE3.REC1.AVISO_TOP          := AVS;
 				{PAGE3.REC1.VLR_IPU
 				PAGE3.REC1.VALOR_ITU
 				PAGE3.REC1.VLR_TOTAL
@@ -99,7 +108,7 @@ while true do Begin
 			WriteRecord(PAGE3,REC1);
 			EndPage(PAGE3);
 			folha := folha+1;
-
+			
 			BeginPage(PAGE4);
 			ClearFields(PAGE4,REC1);
 				PAGE4.REC1.CONTRIBUIN      := contribuinte;
@@ -107,6 +116,7 @@ while true do Begin
 				PAGE4.REC1.IPTU_PREMI:= num_premiado;
 				PAGE4.REC1.CADASTRO := cadastro;
 				PAGE4.REC1.PIX                      := trimStr(getstring(multlineItem(linha_pix,0),31,200));
+				PAGE4.REC1.AVISO_TOP          := AVS;
 			WriteRecord(PAGE4,REC1);
 			EndPage(PAGE4);
 			folha := folha+1;
@@ -123,17 +133,18 @@ while true do Begin
 						PAGE5.REC1.CADASTRO        := cadastro;
 						PAGE5.REC1.DT_UNICA          := FormatDate(getdate(multlineItem(linhas,a),48+(b*bloco_parc),8),'DD/MM/AAAA');
 						PAGE5.REC1.LINHA_DIGI        := getstring(multlineItem(linhas,a),259+(b*bloco_parc),55);
+						PAGE5.REC1.AVISO_TOP          := AVS;
 								if qtd_pix >= 0 then begin
-									if (a = 0) and (b = 0) then begin c := 1; d:= 0;end; // parcela pix 1
-									if (a = 0) and (b = 1) then begin c := 1; d:= 1;end; // parcela pix 2
-									if (a = 0) and (b = 2) then begin c := 1; d:= 2;end; // parcela pix 3
-									if (a = 0) and (b = 3) then begin c := 2; d:= 0;end; // parcela pix 4
-									if (a = 0) and (b = 4) then begin c := 2; d:= 1;end; // parcela pix 5
-									if (a = 1) and (b = 0) then begin c := 2; d:= 2;end; // parcela pix 6
-									if (a = 1) and (b = 1) then begin c := 3; d:= 0;end; // parcela pix 7
-									if (a = 1) and (b = 2) then begin c := 3; d:= 1;end; // parcela pix 8
-									if (a = 1) and (b = 3) then begin c := 3; d:= 2;end; // parcela pix 9
-									if (a = 1) and (b = 4) then begin c := 4; d:= 0;end; // parcela pix 10
+									if (a = 0) and (b = 0) then begin c := 1; d:= 0;end; //parcela pix 1}
+									if (a = 0) and (b = 1) then begin c := 1; d:= 1;end; // parcela pix 2}
+									if (a = 0) and (b = 2) then begin c := 1; d:= 2;end; // parcela pix 3}
+									if (a = 0) and (b = 3) then begin c := 2; d:= 0;end; // parcela pix 4}
+									if (a = 0) and (b = 4) then begin c := 2; d:= 1;end; // parcela pix 5}
+									if (a = 1) and (b = 0) then begin c := 2; d:= 2;end; // parcela pix 6}
+									if (a = 1) and (b = 1) then begin c := 3; d:= 0;end; // parcela pix 7}
+									if (a = 1) and (b = 2) then begin c := 3; d:= 1;end; // parcela pix 8}
+									if (a = 1) and (b = 3) then begin c := 3; d:= 2;end; // parcela pix 9}
+									if (a = 1) and (b = 4) then begin c := 4; d:= 0;end; // parcela pix 10}
 									PAGE5.REC1.PIX                      := trimStr(getstring(multlineItem(linha_pix,c),31+(d*bloco_pix),200));
 								end;
 					WriteRecord(PAGE5,REC1);
@@ -141,12 +152,23 @@ while true do Begin
 					folha := folha+1;
 				end;
 			end;
+			
+			BeginPage(PAGE6);
+			ClearFields(PAGE6,REC1);
+				PAGE6.REC1.CONTRIBUIN      := contribuinte;
+				PAGE6.REC1.INSCRIC         := inscricao;
+				PAGE6.REC1.AVISO_TOP          := AVS;
+			WriteRecord(PAGE6,REC1);
+			EndPage(PAGE6);
+			folha := folha+1;
+			
+		totFls:= folha; {folhas do miolo do carnê}			
 		markup;
-		linhas         := '';
-		linha_pix    :='';
+		linhas      := '';
+		linha_pix   :='';
 		qtdlinhas   := 0;
-		qtd_pix       := 0;
-		impr           := 0;
+		qtd_pix     := 0;
+		impr        := 0;
 	End;
 	
 		If ident = '1' then begin
@@ -175,9 +197,10 @@ while true do Begin
 			qtd_pix      := MultLineCount(linha_pix);
 			impr         := 1;
 		End;//linhas pix única e demais parcelas
-		
 	if close =  1 then Break;
 	if ReadLn(S) = eof then close := 1;
 End;
-abort(FormatNumeric((folha/4),'00000'));
-Convert(4,true,false,false,250,false);
+qtd_caixa:= Ceil(flsCaixa / qtdcarne); {Quantidade de registros por Caixa}
+qtdColun:= Ceil(qtd_caixa/qtdcarne); {Quantidade de registros por Coluna}
+CaixaFls:= (qtdColun*totFls); {Quantidade de Folhas por Caixa}
+Convert(qtdcarne,true,false,false,qtd_caixa,false);
